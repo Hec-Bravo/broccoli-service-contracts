bd9333775e9adee5759a132f9e168234d30ade47
KenatSF <banerabh@gmail.com>
2022-06-22 13:57:55 -0500

    Final detailes added

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,8 @@ import { MoralisProvider } from "react-moralis"
 function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider
-      appId="SXd06lCabGL2vEMiUGo6ltcHQmVB1r8kabTVomOm"
-      serverUrl="https://btos2ep69nlf.usemoralis.com:2053/server"
+      appId="Z1Vrj5PeokKV2uEuyfqqIe34MWXJrIv3b4dAyetq"
+      serverUrl="https://gz8cqdwnzwll.usemoralis.com:2053/server"
     >
       <Component {...pageProps} />
     </MoralisProvider>
