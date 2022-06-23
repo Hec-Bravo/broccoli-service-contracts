e157ef5f93afd92c8dd0d6e7b40914138b856077
KenatSF <banerabh@gmail.com>
2022-06-23 12:17:40 -0500

    appIp & serverUrl updated

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,8 @@ import { MoralisProvider } from "react-moralis"
 function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider
-      appId=""
-      serverUrl=""
+      appId="SfMAKqEEK519WUxMlTuj1ZPbeNTdckKnKWtILykp"
+      serverUrl="https://tobkm26zyzah.usemoralis.com:2053/server"
     >
       <Component {...pageProps} />
     </MoralisProvider>
