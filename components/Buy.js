3ea590381dea37d63739dfab43e7f4234837e385
KenatSF <banerabh@gmail.com>
2022-06-23 16:34:22 -0500

    Minnor failer almost fixed

--- components/Buy.js
+++ components/Buy.js
@@ -2,19 +2,22 @@ import React from "react";
 import { useWeb3Contract } from "react-moralis";
 import { Moralis } from "moralis";
 
-
 export default function Buy() {
-    Moralis.initialize("pk_test_t8xwM7jsQltYEI0Wr9utcREZuSGFF9MqJG5cgq4fux00")
-
+    Moralis.initialize("pk_test_YWj7r9hi2eu3ra1VyBGJUja92nuMK3BzPIyPncIDZew0")
+    Moralis.initPlugins()
 
     function buycrypto() {
+        
         Moralis.Plugins.fiat.buy()
     }
-    
+
 
     return (
         <div>
             <button type="button" onClick={() => buycrypto()} className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Buy USDC</button>
         </div>
     )
+
+    
 }
+
