c87501d22346ab359aa9f552e757a8d0a78c9604
KenatSF <banerabh@gmail.com>
2022-06-23 14:23:22 -0500

    Plugin Buy crypt option added

---
+++ components/BuyCrypto.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useWeb3Contract } from "react-moralis";
+
+import Buy from './Buy';
+
+
+export default function BuyCrypto() {
+
+    return (
+        <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+            <li className="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                <Buy />
+            </li>
+        </ul>
+
+    )
+}
