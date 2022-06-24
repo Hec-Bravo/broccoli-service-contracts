aa0cecdd7f31588f30693b2a449178e695d64b8a
KenatSF <banerabh@gmail.com>
2022-06-24 16:21:19 -0500

    Minnor change at Index.js

--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,9 @@ export default function Home() {
             <button type="button" onClick={logout} className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Disconnect</button>
           </div>
 
+          <FirstAuctionRead /> 
           <Account user={user}/>
-          <FirstAuctionRead />  
+           
 
         </>
         :
